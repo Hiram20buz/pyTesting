68fdc20b814ac1ccb74d165894092331e01b88c3
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-30 21:56:36 -0700

    Add files via upload

---
+++ pyTEST/a.py
@@ -0,0 +1,37 @@
+import pytest
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Division by zero is not allowed")
+        return a / b
+
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+def test_add(calculator):
+    assert calculator.add(2, 3) == 5
+
+def test_subtract(calculator):
+    assert calculator.subtract(5, 3) == 2
+
+def test_multiply(calculator):
+    assert calculator.multiply(4, 3) == 12
+
+def test_divide(calculator):
+    assert calculator.divide(10, 2) == 5
+
+def test_divide_by_zero(calculator):
+    with pytest.raises(ValueError):
+        calculator.divide(10, 0)
+
